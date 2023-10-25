c495fd821e87b3e7dfb9254d6241391a20d35308
jimklo83 <jimklo83@mail.com>
2023-10-25 09:45:23 -0500

    FizzBuzz

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FizzBuzz 
+{
+    class Program 
+    {
+        string FizzBuzz(int number) 
+        {
+            if (number % 3 == 0 && number % 5 == 0) 
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 3 == 0) 
+            {
+                return "Fizz";
+            }
+            else if (number % 5 == 0) 
+            {
+                return "Buzz";
+            }
+            else 
+            {
+                return number.ToString();
+            }
+        }
+
+        Console.WriteLine(FizzBuzz(15));
+    }
+}
+
+
