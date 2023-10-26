f5fb8d42ff144aaae7975339f8ce4d875331a48a
jimklo83 <jimklo83@mail.com>
2023-10-26 11:41:37 -0500

    Added and array method

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 {
     public class Program 
     {
+        //FizzBuzz that takes in an integer as parameter and returns a string
         public string FizzBuzz(int number) 
         {
             if (number % 3 == 0 && number % 5 == 0) 
@@ -22,6 +23,32 @@
             }
         }
 
+        //FizzBuzz that takes in an integer array as parameter and returns a string array
+        public string[] FizzBuzz(int[] array) 
+        {
+            var fizzBuzz = new string[array.Length];
+
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (array[i] % 3 == 0 && array[i] % 5 == 0) 
+                {
+                    fizzBuzz[i] = "FizzBuzz";
+                }
+                else if (array[i] % 3 == 0) 
+                {
+                    fizzBuzz[i] = "Fizz";
+                }
+                else if (array[i] % 5 == 0) 
+                {
+                    fizzBuzz[i] = "Buzz";
+                }
+                else 
+                {
+                    fizzBuzz[i] = array[i].ToString();
+                }
+            }
+            return fizzBuzz;
+        }
     }
 }
 
